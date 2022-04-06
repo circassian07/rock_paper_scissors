ed7749f87f4a2d21a0bea431e1574ef889672f52
Kantemir <kantemir.kertiev@hotmail.com>
2022-04-06 18:03:54 -0400

    added Draw option

--- game.js/game.js
+++ game.js/game.js
@@ -2,5 +2,20 @@ function computerPlay(play) {
     return play[Math.floor(Math.random()*play.length)]
 }
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log("It's a draw!")
+    }
+    else if (playerSelection === "Rock" && computerSelection === "Scissors") {}
+    else if (playerSelection === "Rock" && computerSelection === "Paper") {}
+    else if (playerSelection === "Paper" && computerSelection === "Rock") {}
+    else if (playerSelection === "Paper" && computerSelection === "Scissors") {}
+    else if (playerSelection === "Scissors" && computerSelection === "Paper") {}
+    else if (playerSelection === "Scissors" && computerSelection === "Rock") {}
+}
+
 let play = ['Rock', 'Paper', 'Scissors'];
-console.log(computerPlay(play));
+let computerSelection = computerPlay(play);
+let playerSelection = prompt('Enter your pick: ');
+console.log(computerSelection);
+playRound(playerSelection, computerSelection);
