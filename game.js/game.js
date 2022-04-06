582c3c4797147bf13b04a20e2f5bbf15f6e693d6
Kantemir <kantemir.kertiev@hotmail.com>
2022-04-06 18:28:24 -0400

    playRound complete

--- game.js/game.js
+++ game.js/game.js
@@ -6,16 +6,30 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         console.log("It's a draw!")
     }
-    else if (playerSelection === "Rock" && computerSelection === "Scissors") {}
-    else if (playerSelection === "Rock" && computerSelection === "Paper") {}
-    else if (playerSelection === "Paper" && computerSelection === "Rock") {}
-    else if (playerSelection === "Paper" && computerSelection === "Scissors") {}
-    else if (playerSelection === "Scissors" && computerSelection === "Paper") {}
-    else if (playerSelection === "Scissors" && computerSelection === "Rock") {}
+    else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        console.log("You win! Rock beats Scissors!");
+    }
+    else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        console.log("You lose! Paper beats Rock!");
+    }
+    else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        console.log("You win! Paper beats Rocks!");
+    }
+    else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        console.log("You lose! Scissors beat Paper!");
+    }
+    else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        console.log("You win! Scissors beat Paper!");
+    }
+    else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        console.log("You lose! Rock beats Scissors!");
+    }
 }
 
 let play = ['Rock', 'Paper', 'Scissors'];
 let computerSelection = computerPlay(play);
 let playerSelection = prompt('Enter your pick: ');
+let scorePlayer = 0;
+let scoreComputer = 0;
 console.log(computerSelection);
 playRound(playerSelection, computerSelection);
