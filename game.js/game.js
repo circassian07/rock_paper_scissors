52c2ee7d2a296a9638517fa2b45c4eedfb0c18bf
Kantemir <kantemir.kertiev@hotmail.com>
2022-04-18 15:25:46 -0400

    added running score and game up to 5 pts

--- game.js/game.js
+++ game.js/game.js
@@ -1,58 +1,84 @@
+let play = ['Rock', 'Paper', 'Scissors'];
+let scorePlayer = 0;
+let scoreComputer = 0;
+
+
 function computerPlay(play) {
     return play[Math.floor(Math.random()*play.length)]
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        console.log("It's a draw!");
+        displayScore.textContent = `It's a draw!\n Score: ${scorePlayer} vs ${scoreComputer}`;
     }
     else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        console.log("You win! Rock beats Scissors!");
-        return scorePlayer++;
+        scorePlayer++;
+        displayScore.textContent = `You win! Rock beats Scissors!\n Score: ${scorePlayer} vs ${scoreComputer}`;
+        return scorePlayer;
     }
     else if (playerSelection === "Rock" && computerSelection === "Paper") {
-        console.log("You lose! Paper beats Rock!");
-        return scoreComputer++;
+        scoreComputer++;
+        displayScore.textContent = `You lose! Paper beats Rock!\n Score: ${scorePlayer} vs ${scoreComputer}`;
+        return scoreComputer;
     }
     else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        console.log("You win! Paper beats Rocks!");
-        return scorePlayer++;
+        scorePlayer++;
+        displayScore.textContent = `You win! Paper beats Rocks!\n Score: ${scorePlayer} vs ${scoreComputer}`;
+        return scorePlayer;
     }
     else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-        console.log("You lose! Scissors beat Paper!");
-        return scoreComputer++;
+        scoreComputer++;
+        displayScore.textContent = `You lose! Scissors beat Paper!\n Score: ${scorePlayer} vs ${scoreComputer}`;
+        return scoreComputer;
     }
     else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-        console.log("You win! Scissors beat Paper!");
-        return scorePlayer++;
+        scorePlayer++;
+        displayScore.textContent = `You win! Scissors beat Paper!\n Score: ${scorePlayer} vs ${scoreComputer}`;
+        return scorePlayer;
     }
     else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        console.log("You lose! Rock beats Scissors!");
-        return scoreComputer++;
-    }
-}
-
-function game() {
-    for (let i=0;i<5;i++) {
-        let computerSelection = computerPlay(play);
-        let playerSelection = prompt('Enter your pick: ');
-        console.log(computerSelection);
-        playRound(playerSelection, computerSelection);
+        scoreComputer++;
+        displayScore.textContent = `You lose! Rock beats Scissors!\n Score: ${scorePlayer} vs ${scoreComputer}`;
+        return scoreComputer;
     }
 }
 
 function results() {
-    if (scorePlayer>scoreComputer) {
-        console.log(`Congrats! You won the game! Final score is ${scorePlayer} & ${scoreComputer}`)
-    } else if (scoreComputer>scorePlayer) {
-        console.log(`Sorry, you lose! Better luck next time! Final score is ${scorePlayer} & ${scoreComputer}`)
-    }
+    if (scorePlayer === 5 && scorePlayer>scoreComputer) {
+        displayScore.textContent = `Congrats! You won the game! Final score is ${scorePlayer} & ${scoreComputer}`
+    } else if (scoreComputer === 5 && scoreComputer>scorePlayer) {
+        displayScore.textContent = `Sorry, you lose! Better luck next time! Final score is ${scorePlayer} & ${scoreComputer}`
+    } 
 }
 
-let play = ['Rock', 'Paper', 'Scissors'];
-let scorePlayer = 0;
-let scoreComputer = 0;
+const rock = document.getElementById('rock')
+const paper = document.getElementById('paper')
+const scissors = document.getElementById('scissors')
+
+var displayScore = document.getElementById('results');
 
 
-game();
+
+
+rock.addEventListener('click',function() {
+    let computerSelection = computerPlay(play);
+    let playerSelection = 'Rock';
+    playRound(playerSelection, computerSelection)
+    results(scorePlayer, scoreComputer)
+})
+
+paper.addEventListener('click',function(){
+    let computerSelection = computerPlay(play);
+    let playerSelection = 'Paper';
+    playRound(playerSelection, computerSelection)
+    results(scorePlayer, scoreComputer)
+})
+scissors.addEventListener('click',() => {
+    let computerSelection = computerPlay(play);
+    let playerSelection = 'Scissors';
+    playRound(playerSelection, computerSelection)
+    results(scorePlayer, scoreComputer)
+})
+
+
 results();
